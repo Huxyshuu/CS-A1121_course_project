779b556134cc9361d5e2e3b65cd5063525ad61d6
Huxyshuu <hugotamm@hotmail.com>
2023-03-12 21:44:41 +0200

    A window pop up with a functioning button

--- Code/main.py
+++ Code/main.py
@@ -1,1 +1,34 @@
-print("Hello Mom!")
+import sys
+
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt6.QtGui import QPalette, QColor
+
+class MainWindow(QMainWindow): # Creating a subclass of QMainWindow for better control over the window customization
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Pipeflow - Hugo")
+
+        self.setFixedSize(QSize(1280, 800)) # Sets a fixed window size
+
+        self.button = QPushButton("Press Me!")
+        self.button.setFixedSize(QSize(200, 200))
+        # widget.clicked.connect(func) # Add functionality
+        self.button.clicked.connect(self.printClick) # PyCharm showing error on .connect  # it works regardless
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(self.button)
+
+    def printClick(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show() # Makes the window visible
+
+app.exec()

d5f4a8017f1b67567b13a46da84a9fc6e9307e1b
Huxyshuu <hugotamm@hotmail.com>
2023-03-18 13:52:23 +0200

    Added a pipe adding feature to grid.

--- Code/grid.py
+++ Code/grid.py
@@ -9,24 +9,26 @@ from gridSquare import GridSquare
 class Grid(QGraphicsView):
     def __init__(self, width, height, square_size):
         super(Grid, self).__init__()
-        self.setFixedSize(width, height)
+        # whitespace_x and _y remove empty space between the grid and the border
+        whitespace_x = width % square_size
+        whitespace_y = height % square_size
+        self.setFixedSize(width - whitespace_x, height-whitespace_y)
 
         self.scene = QGraphicsScene()
         # (1, 1) in the beginning position the center of the grid so that the sides are not visible
         self.scene.setSceneRect(1, 1, width-square_size, height-square_size)
 
+        # Line for testing purposes
         self.line = QGraphicsLineItem()
         self.line.setLine(0, 0, 100, 100)
 
         self.scene.addItem(self.line)
 
+        # Creates a GridSquare object for every position in the grid
         for x in range(floor(width/square_size)):
             for y in range(floor(height/square_size)):
                 sq = GridSquare(x * square_size, y * square_size, square_size, square_size)
                 self.scene.addItem(sq)
-
-
-
 
         self.setScene(self.scene)
         self.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
--- Code/gridSquare.py
+++ Code/gridSquare.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QGraphicsRectItem
+from PyQt6 import QtWidgets, QtGui
 
 
 class GridSquare(QGraphicsRectItem):
@@ -7,6 +8,22 @@ class GridSquare(QGraphicsRectItem):
         # coordinates are divided by the size of the box for easier readability, (1, 1) instead of (w, h)
         self.x = x / w
         self.y = y / h
+        self.__empty = True # Initialize empty square
+        self.setBrush(QtGui.QColor(255, 255, 255)) # Default color
+
+    def is_empty(self):
+        return self.__empty
+
+    def setPipe(self):
+        self.__empty = False
+        self.setBrush(QtGui.QColor(20, 20, 20))
+
+    def removePipe(self):
+        self.__empty = True
+        self.setBrush(QtGui.QColor(255, 255, 255))
 
     def mousePressEvent(self, event: 'QGraphicsSceneMouseEvent'):
-        print("Clicked a square at ({}, {})".format(self.x, self.y))
+        if self.is_empty():
+            self.setPipe()
+        else:
+            self.removePipe()
